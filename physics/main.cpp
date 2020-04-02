8a24dbc18a5bb0c352925459c10c42548b8664d8
Joshua Francis <joshua.francis@live.com.au>
2020-04-02 21:38:20 +1100

    Added empty project

---
+++ physics/main.cpp
@@ -0,0 +1,15 @@
+#include "physicsApp.h"
+
+int main() {
+	
+	// allocation
+	auto app = new physicsApp();
+
+	// initialise and loop
+	app->run("AIE", 1280, 720, false);
+
+	// deallocation
+	delete app;
+
+	return 0;
+}
